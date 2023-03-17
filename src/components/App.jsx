46d02b6215dc27d2f13547ec5ff8f7f6787c5abe
Maksym-Plutenko <plutenkom@gmail.com>
2023-03-17 22:44:50 +0200

    app structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { Routes, Route, NavLink } from "react-router-dom";
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+      <Routes>
+        <Route path="/" element={<SharedLayout />} >
+          <Route element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 };
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,12 @@
+const SharedLayout = () => {
+  return (
+    <header>
+      <nav>
+        <NavLink>Home</NavLink>
+        <NavLink>Movies</NavLink>
+      </nav>
+    </header>
+  );
+};
+
+// active
