9f0d6d6d404ea2f08a02ff1dc25601f139eb3937
Maksym-Plutenko <plutenkom@gmail.com>
2023-03-19 19:14:41 +0200

    seachParam

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 import apiserver from '../../utilites/apiserver';
 import Filmlist from '../../components/Filmlist/Filmlist';
@@ -7,19 +8,34 @@ import Filmlist from '../../components/Filmlist/Filmlist';
 const Movies = () => {
   const [request, setRequest] = useState('');
   const [films, setFilms] = useState([]);
+  const [seachParam, setSeachParam] = useSearchParams({});
+  const seachRequest = seachParam.get('request');
+
+  useEffect(() => {
+    async function filmSeach(request) {
+      const response = await apiserver.seach(request);
+      setFilms(response);
+    }
+
+    if (seachRequest) {
+      filmSeach(seachRequest);
+    }
+  }, [seachRequest]);
 
   const onChange = evt => {
     setRequest(evt.currentTarget.value);
   };
 
   const onSubmit = evt => {
-    async function filmSeach(request) {
-      const response = await apiserver.seach(request);
-      setFilms(response);
-    }
+    // async function filmSeach(request) {
+    //   const response = await apiserver.seach(request);
+    //   setFilms(response);
+    // }
 
     evt.preventDefault();
-    filmSeach(request);
+    // filmSeach(request);
+    setSeachParam({request});
+    setRequest('');
   };
 
   return (
